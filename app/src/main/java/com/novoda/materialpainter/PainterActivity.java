3437d6ba7f3d5623caa276a3873e8a1e4ba2859a
David Gonzalez <malmstein@gmail.com>
2015-02-22 19:44:16 +0000

    initial functionality, lots of things missing

--- app/src/main/java/com/novoda/materialpainter/PainterActivity.java
+++ app/src/main/java/com/novoda/materialpainter/PainterActivity.java
@@ -1,37 +1,98 @@
 package com.novoda.materialpainter;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.ParcelFileDescriptor;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.graphics.Palette;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.novoda.notils.caster.Views;
+
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class PainterActivity extends ActionBarActivity {
+
+    private static final int READ_REQUEST_CODE = 42;
+
+    private ImageButton selectImage;
+    private ImageView imageView;
+
+    private View mutedLayout;
+    private TextView mutedTitle;
+    private TextView mutedBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_painter);
+
+        mutedLayout = findViewById(R.id.muted_layout);
+        mutedTitle = Views.findById(this, R.id.muted_layout_text);
+        mutedBody = Views.findById(this, R.id.muted_layout_body_text);
+
+        imageView = Views.findById(this, R.id.show_image);
+        selectImage = Views.findById(this, R.id.fab_select_image);
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performFileSearch();
+            }
+        });
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_painter, menu);
-        return true;
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri;
+            if (resultData != null) {
+                uri = resultData.getData();
+                showImage(uri);
+            }
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void performFileSearch() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    private void showImage(Uri uri) {
+        ParcelFileDescriptor parcelFileDescriptor;
+        try {
+            parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+            imageView.setImageBitmap(image);
+
+            Palette.generateAsync(image, new Palette.PaletteAsyncListener() {
+                public void onGenerated(Palette palette) {
+                    Palette.Swatch mutedSwatch = palette.getMutedSwatch();
+                    mutedLayout.setBackgroundColor(mutedSwatch.getRgb());
+                    mutedTitle.setTextColor(mutedSwatch.getTitleTextColor());
+                    mutedBody.setTextColor(mutedSwatch.getBodyTextColor());
+                }
+            });
+
+            parcelFileDescriptor.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
